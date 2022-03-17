b3a8be0ab1a15ebc278c825415a0e9475f8e8fff
Эльдар Алиев <alel29643@gmail.com>
2022-03-17 22:56:52 +0300

    доработка

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,14 @@
 fun main() {
-    val likes = (1..200).random()
-    val people =  if (likes == 1 || (likes%10 == 1 && likes != 11)) "человеку" else "людям"
+
+    val likes = (1..1000).random()
+
+    val people =
+        if (likes % 100 == 11)
+            "людям"
+        else if (likes == 1 || likes % 10 == 1 && likes != 11)
+            "человеку"
+        else
+            "людям"
+
     println("Понравилось $likes $people")
 }
